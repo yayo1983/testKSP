e24106ed46efa4d441e97a9f036926226e229ece
yasser <yazanenator@gmail.com>
2023-02-03 14:18:29 -0600

    Adicionando templatetag

---
+++ employee/templatetags/djutils.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+import json
+
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter('get_value_from_dict')
+def get_value_from_dict(dict_data, key):
+    """
+    usage example {{ your_dict|get_value_from_dict:your_key }}
+    """
+    if key:
+        return dict_data.get(key)
+
+
+@register.filter
+def get_by_name(enum, key):
+    return getattr(enum, key).value
